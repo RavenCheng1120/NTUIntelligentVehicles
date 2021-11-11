fb1cf15a311fb2d5e025e06d4a5e7b7dd2132338
RavenCheng1120 <ravencheng66@gmail.com>
2021-11-12 04:55:08 +0800

    hw2 temp solution

--- Homework2/hw2.py
+++ Homework2/hw2.py
@@ -1,6 +1,159 @@
-import hw1
+import math
+import random
+import sys
+import copy
+
+# Read the input.dat file. Get the total number of the messages, tau, and lines contains the priority (Pi), the transmission time (Ci), and the period (Ti)
+def ImportData(filename):
+	n = open(filename).readlines()[:1]
+	tau = open(filename).readlines()[1:2]
+	n = int(''.join(n).strip())
+	tau = float(''.join(tau).strip())
+	dataList = [i.strip().split() for i in open(filename).readlines()[2:]] # read data to a 2D list
+	dataList = [list(map(float, data)) for data in dataList] # turn type string to float
+
+	return n, tau, dataList
+
+def PriorityFindMessageLocation(prior, localDataList, n):
+	idx = 0
+	for x in range(n):
+		if localDataList[x][0] == prior:
+			idx = x
+	return idx
+
+
+def CalculateResponse(index, n, tau, localDataList):
+	Q, B, R = 0, 0, 0
+	isViolate = False
+	
+	# Find the B value (blocking time of the longest lower or same priority message).
+	for blockIndex in (lower for lower in [firstColumn[0] for firstColumn in localDataList] if lower >= localDataList[index][0]):
+		location = PriorityFindMessageLocation(int(blockIndex), localDataList, n)
+		if B < localDataList[location][1]:
+			B = localDataList[location][1]
+
+	Q = B
+	while True:
+		sum = 0
+		for blockIndex in (lower for lower in [firstColumn[0] for firstColumn in localDataList] if lower < localDataList[index][0]):
+			location = PriorityFindMessageLocation(int(blockIndex), localDataList, n)
+			sum += math.ceil((Q + tau) / localDataList[location][2]) * localDataList[location][1]
+
+		if (B+sum)+localDataList[index][1] > localDataList[index][2] and not isViolate:
+			# print("Constraint violation")
+			isViolate = True
+		
+		if Q == B+sum:
+			# worst-case R
+			R = round((B+sum)+localDataList[index][1],2)
+			# print(f'{ R }')
+			break
+		else:
+			Q = B+sum
+
+	return float(R), isViolate
+
+
+def SwapPriority(n, localDataList):
+	tempDataList = copy.deepcopy(localDataList)
+	# Random select two priority to swap
+	x, y = random.sample(priorityList, 2)
+	tempDataList[x][0], tempDataList[y][0] = tempDataList[y][0], tempDataList[x][0] # Swap two messages' priority
+	print(f'Swap {x} and {y}')
+	return tempDataList
+
+def SwapPriorityNear(n, localDataList):
+	tempDataList = copy.deepcopy(localDataList)
+	# Random select two priority to swap
+	x = random.choice(priorityList)
+	if int(x) < n-1:
+		tempDataList[x][0], tempDataList[x+1][0] = tempDataList[x+1][0], tempDataList[x][0] # Swap two messages' priority
+	else:
+		tempDataList[x][0], tempDataList[x-1][0] = tempDataList[x-1][0], tempDataList[x][0]
+	print(f'Swap {x} and {x+1}')
+	return tempDataList
+
 
 if __name__ == '__main__':
-	n, tau, dataList = hw1.importData("input.dat")
+	# Read in the data set
+	n, tau, dataList = ImportData("input.dat")
+	priorityList = [int(dataListPriority[0]) for dataListPriority in dataList]
+
+	# Summation of the worst-case response times of all messages. The objective is to minimize it.
+	summation, newSummation = 0, 0
+	T = 100000000
+	reduceRate = 0.4
+	isResponseViolate = False
+	finalOutput = 0
+
+	# Calculate the response time of each message
 	for i in range(int(n)):
-		hw1.calculateResponse(i, n, tau, dataList)
+		idx = PriorityFindMessageLocation(i, dataList, n)
+		temp_summation, isResponseViolate = CalculateResponse(idx, n, tau, dataList)
+		summation += temp_summation
+		if isResponseViolate:
+			print("Constraint violation")
+			sys.exit()
+	finalOutput = summation
+	print(f'Origin sum = {summation}\n')
+
+
+	while T > 0:
+		newDataList = SwapPriority(n, dataList)
+		# newDataList = SwapPriorityNear(n, dataList)
+		tempR = 0
+		newSummation = 0
+		isResponseViolate = False
+
+		# Calculate the response time of each message
+		for i in range(int(n)):
+			idx = PriorityFindMessageLocation(i, newDataList, n)
+			tempR, temp_isViolate = CalculateResponse(idx, n, tau, newDataList)
+			if temp_isViolate:
+				isResponseViolate = True
+			newSummation += tempR
+
+		newSummation = round(newSummation, 2)
+
+		# "down-hill" move
+		if newSummation <= summation:
+			# Violated the constraint, make it harder to go down hill in this direction
+			if isResponseViolate:
+				print("Constraint violation")
+				T = T * reduceRate
+				continue
+				# prob = min(math.exp(-(summation-newSummation)*10000/T), 1)
+				# if random.random() <= prob:
+				# 	print()
+				# 	continue
+
+			dataList = copy.deepcopy(newDataList)
+			summation = newSummation
+			print(f'downhill: {summation}')
+
+			if isResponseViolate == False and finalOutput > summation:
+				finalOutput = summation
+			print()
+		# "up-hill" move
+		else:
+			prob = min(math.exp(-(newSummation-summation)/T), 1)
+			# if isResponseViolate:
+			# 	prob = prob / 2
+			print(f'\tSum = {summation} New Sum = {newSummation} / Prob = {prob}')
+			
+			# Take the chance to go up hill
+			if random.random() <= prob:
+				dataList = copy.deepcopy(newDataList)
+				summation = newSummation
+				print(f'\tuphill: {summation}')
+				# print(f'\t{dataList}')
+		print()
+
+		T = T * reduceRate
+
+	for i in range(int(n)):
+		print(int(dataList[i][0]))
+	print(finalOutput)
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# NTUIntelligentVehicles
+# [NTU 2021 Fall] Intelligent Vehicles
+
+智慧型汽車導論程式作業
+
+## Homework 2
+
+In the benchmark, the first number is n, the number of messages. The second number is τ . Each of the following lines contains the priority (Pi), the transmission time (Ci), and the period (Ti) of each message. 
+
+Now, you are asked to use the Simulated Annealing to decide the priority of each message.
+
+
+
+**The requirements are: ** 
+
+- The objective is to minimize the summation of the worst-case response times of all messages.  
+- The priority of each message must be an integer in the range [0, n − 1].  
+- The priority of each message must be unique.  
+- The worst-case response time of each message must be smaller than or equal to the period of each message.  
+- The given priorities are the initial solution in the Simulated Annealing.  
+- We expect the total runtime less than 15 seconds.
